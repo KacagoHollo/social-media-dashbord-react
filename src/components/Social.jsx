95032f8d5f5a81a402faf03998eb5740de819eb1
IstvanPongracz <pongracz88@gmail.com>
2022-08-21 18:49:48 +0200

    platform name changes

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,13 +1,16 @@
 import React, {useState} from 'react'
 
 function Social({platform}) {
+  const platName = (Object.keys(platform)[0]);
+  console.log(platName)
   return (
       <>
         {Object.values(platform).map((profile, i) => (
           <div className='social' key={i}>
-            <h2>{profile.name}</h2>
+            <h2>{platName === "youtube" ? profile.name : "@" + profile.name}</h2>
             <h1>{profile.followers}</h1>
-            <h3>{profile.difference}</h3>
+            <h1>FOLLOWERS</h1>
+            <h3>{profile.difference} Today</h3>
           </div>
         ))}
       </>
