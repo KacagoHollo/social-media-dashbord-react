ed0858c29951a03d16c1b976861808485b81657c
IstvanPongracz <pongracz88@gmail.com>
2022-09-08 20:26:33 +0200

    color, mui, arrow placed, minus symbol out

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,5 +1,7 @@
-import React, {useState} from 'react';
+import React from 'react';
 import smIcons from '../data/smicons';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
 
 function Social({platform}) {
   const platName = (Object.keys(platform)[0]);
@@ -19,7 +21,8 @@ function Social({platform}) {
             <h2> <span className='icons'>{smIcons[platName]}</span>{platName === "youtube" ? profile.name : "@" + profile.name}</h2>
             <h1>{profile.followers >= 1e4 ? (profile.followers / 1e3).toFixed(0) + "k" : profile.followers}</h1>
             <h1>{platName === "youtube" ? "SUBSCRIBERS" : "FOLLOWERS"}</h1>
-            <h3 className={profile.difference > 0 ? "green" : "red"}>{profile.difference} Today</h3>
+            <h3 style={{ display: 'flex', alignItems: 'center' }} className={profile.difference > 0 ? "green" : "red"}>{profile.difference > 0 ? <ArrowDropUpIcon  style={{ color: "#46938c" }}></ArrowDropUpIcon> : <ArrowDropDownIcon style={{ color: "#b0455c" }}></ArrowDropDownIcon>}{Math.abs(profile.difference)} Today</h3>
+            
           </div>
         ))}
       </>
