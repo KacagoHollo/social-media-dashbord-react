b380a172a9d13d308f01e3c9135fe1521344d774
IstvanPongracz <pongracz88@gmail.com>
2022-09-09 22:54:53 +0200

    arrow position/color

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -18,7 +18,7 @@ function Overview({event}) {
             <h1><span className='icons'>{smIcons[platName]}</span></h1>
             <h2 className='type'>{event.type}</h2>
             <h2 className='amount'>{event.amount}</h2>
-            <p className={event.modifier > 0 ? "green" : "red"}>{event.modifier + "%"}</p>
+            <p style={{ display: 'flex', alignItems: 'center' }} className={event.modifier > 0 ? "green" : "red"}>{event.modifier > 0 ? <ArrowDropUpIcon  style={{ color: "#46938c" }}></ArrowDropUpIcon> : <ArrowDropDownIcon style={{ color: "#b0455c" }}></ArrowDropDownIcon>}{Math.abs(event.modifier) + "%"}</p>
         </div>
     </>
   )
