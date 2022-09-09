544d1a08aee89360e13119a7b4e6c036c6e92285
IstvanPongracz <pongracz88@gmail.com>
2022-09-09 22:27:56 +0200

    overview card positions

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React from 'react';
+import smIcons from '../data/smicons';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
 
 function Overview({event}) {
+  console.log(event)
+  const platName = event.platform;
+
+  Intl.NumberFormat('en-US', {
+    notation: "compact",
+    maximumFractionDigits: 1
+  }).format(10000);
+
   return (
     <>
         <div className='over'>
-          <h1>{event.platform}</h1>
-          <h2>{event.type}</h2>
-          <h2>{event.amount}</h2>
-          <p>{event.modifier}</p>
+            <h1><span className='icons'>{smIcons[platName]}</span></h1>
+            <h2 className='type'>{event.type}</h2>
+            <h2 className='amount'>{event.amount}</h2>
+            <p>{event.modifier + "%"}</p>
         </div>
     </>
   )
